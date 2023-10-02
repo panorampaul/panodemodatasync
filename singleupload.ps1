1d1f9c2c028c2540ddfaaa5de9a6c099b58ed757
Paul <paul@sharpred.com>
2023-10-02 16:20:28 +0100

    remove test file

--- singleupload.ps1
+++
@@ -1,4 +0,0 @@
-function Test-AddFile {
-  param()
-  Add-File -FilePath Downloads/testfile.docx -FileName test3.docx -FolderId clauses -SiteId panoramdigitalltd.sharepoint.com,47d671ba-6549-498b-ab5a-5d0739ae77d4,5a4554df-d873-4769-af82-0c9df2f318b4
-}
